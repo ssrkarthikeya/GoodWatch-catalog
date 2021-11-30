4b1698acd3c36e9cc564592a38ca4b640b258fab
ssrkarthikeya <Hachinov1>
2021-11-30 11:35:45 +0530

    initial

---
+++ src/main/java/io/icegadda/goodwatchcatalogservice/GoodwatchCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package io.icegadda.goodwatchcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodwatchCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodwatchCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/icegadda/goodwatchcatalogservice/GoodwatchCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.icegadda.goodwatchcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodwatchCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
